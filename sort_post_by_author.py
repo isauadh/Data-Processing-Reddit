a0d260964e3350722312fabe4d24f4593a63c847
Saugat Adhikari <33466771+isauadh@users.noreply.github.com>
2019-03-24 16:14:13 -0500

    Create sort_post_by_author.py

---
+++ sort_post_by_author.py
@@ -0,0 +1,41 @@
+import praw
+import json
+
+reddit = praw.Reddit(client_id='', \
+                     client_secret='', \
+                     user_agent='', \
+                     username='isauadh', \
+                     password='')
+def writeData(fp, data):
+    outputfp = open(fp, "w")
+    outputfp.write(json.dumps(data, sort_keys = True, indent = 4))
+
+def Subreddit(name, limitTo):
+  subreddit = reddit.subreddit('opiates')
+  submissions = subreddit.top(limit = 1) 
+  posts = {}
+  
+  for submission in submissions:
+    flag_sub = 0
+    for key in posts.keys():
+      if submission.author == key:
+        posts[key].append(submission.selftext)
+        flag_sub = 1
+    if flag_sub == 0:
+      posts[str(submission.author)] = []
+      posts[str(submission.author)].append(submission.selftext)
+    for comment in submission.comments.list():
+      flag = 0
+      for key in posts.keys():
+        if comment.author == key:
+          posts[key].append(comment.body)
+          flag = 1
+          break
+      if flag==0:    
+        posts[str(comment.author)] = []
+        posts[str(comment.author)].append(comment.body)
+        
+  writeData("{}.txt".format('Post1'),posts)
+  
+ if __name__ == '__main__':
+  Subreddit('opiates', None)
