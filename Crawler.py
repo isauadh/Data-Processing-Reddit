08668e4e4bf2118ee827ac2d8d468927530fa59a
Saugat Adhikari <33466771+isauadh@users.noreply.github.com>
2019-03-30 12:36:24 -0500

    Update Crawler.py

--- Crawler.py
+++ Crawler.py
@@ -1,11 +1,11 @@
 import praw
 import json
 
-reddit = praw.Reddit(client_id='client id', \
-                     client_secret='client secret', \
-                     user_agent='user agent', \
+reddit = praw.Reddit(client_id='IFHDQg4dnEQ2AA', \
+                     client_secret='bVZAdZRh6XPPn6G0R-kChVsoEMo', \
+                     user_agent='tangocharlie1', \
                      username='isauadh', \
-                     password='password')
+                     password='s@ugaT1320')
 
 def Subreddit(name, limitTO):
     print("Subreddit {}".format(name))
