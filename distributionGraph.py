4cd6674f686f8646affdde76b00135fefb7fb135
Saugat Adhikari <33466771+isauadh@users.noreply.github.com>
2019-04-09 18:08:15 -0500

    Update distributionGraph.py

--- distributionGraph.py
+++ distributionGraph.py
@@ -13,10 +13,12 @@ reddit = praw.Reddit(client_id='IFHDQg4dnEQ2AA', \
                      username='isauadh', \
                      password='s@ugaT1320')
 
+# create a file with json objects
 def writeData(fp, data):
   outputfp = open(fp, "w")
   outputfp.write(json.dumps(data, sort_keys = True, indent = 4))
-    
+
+# function used to hide the redditor name with changes in first two, middle, and last two letters of the redditor name
 def hideUserid(name):
   s = list(name)
   length = len(name)
@@ -27,22 +29,22 @@ def hideUserid(name):
   s[(length-2)] = random.choice(string.ascii_lowercase)
   return ("".join(s))
 
+# create a directory with the necessary files in it
 def makeDir(name, data):
   os.mkdir(name)
-  #writeData("{}.txt".format(os.path.join(name, name)), data)
   for key in data.keys():
     writeData("{}.txt".format(os.path.join(name, hideUserid(key))), data[key])
-    #writeData("{}.txt".format(os.path.join(name, key)), data[key])
 
+# create a bar diagram to analyze frequency of the each bin(post count)
 def createDistribution(data):
     graphingData = {}
     for key in data.keys():
         graphingData[key] = len(data[key][0]['post'])
     df = pd.DataFrame.from_dict(graphingData, orient = 'index', columns = ['count'])
-    #bins = [0,4,8,12,16,20,24,28,32,36,40,44,48,52,56,60,64,68,72,76,80]
     df['binned'] = pd.cut(df['count'], bins = 150)
-    print(df['binned'].value_counts().plot.bar())
+    print(df['binned'].value_counts().plot.bar(figsize =(20,10))
 
+# create a bar diagram to analyze time distribution of a redditor in a given subreddit
 def timeDistribution(authorName, data):
     graphingData = {}
     graphingData[authorName] = []
@@ -52,15 +54,15 @@ def timeDistribution(authorName, data):
     df1 = pd.DataFrame.from_dict(graphingData) 
     bins = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
     df1['binned'] = pd.cut(df1[authorName], bins = bins)
-    print(df1['binned'].value_counts().plot.bar())
+    print(df1['binned'].value_counts().plot.bar(figsize=(20,10))
 
+# gather posts, comments, and their posted time off the top 1k submissions of all time of a subreddit 
+# gathered information are sorted by redditor in the given subreddit
 def Subreddit(name, limitTo):
-      count = 0
       redditors = {}
       subreddit = reddit.subreddit(name)
       submissions = subreddit.top(limit = limitTo)
       for submission in submissions:
-        print(count)
         flag_sub = 0
         for key in redditors.keys():
           if submission.author == key:
@@ -88,18 +90,12 @@ def Subreddit(name, limitTo):
             redditors[str(comment.author)][0]['postTime'] = []
             redditors[str(comment.author)][0]['post'].append(comment.body)
             redditors[str(comment.author)][0]['postTime'].append(comment.created_utc)
-        count += 1
-      print("Done..")
-      makeDir(name, redditors)
       return redditors
     
 if __name__ == '__main__':
-  allPost = Subreddit('OpiatesRecovery', None)
-  while(True):
-    User = input("Time distribution of the user: ")
-    timeDistribution(User, allPost)
-    proceed = input("Press 'y' to view more time distribution or 'n' to stop: ")
-    if proceed == y:
-      continue
-    else:
-      break
+  # Please provide the subreddit name before running the program
+  allPost = Subreddit('Subreddit_Name', None)
+  # Please provide the directory name to store the gathered information
+  makeDir('Directory_Name', allPost)
+  # Please provide the redditor name to view the time distribution of his/her posts in the given subreddit
+  timeDistribution('Redditor_Name', allPost)       
