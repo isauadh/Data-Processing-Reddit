c422a8b65c6d98c41b699a772ab783cf29493226
Saugat Adhikari <33466771+isauadh@users.noreply.github.com>
2019-04-09 19:44:55 -0500

    Update RedditorPost-Subreddit.py

--- RedditorPost-Subreddit.py
+++ RedditorPost-Subreddit.py
@@ -11,35 +11,41 @@ def writeData(fp, data):
     outputfp = open(fp, "w")
     outputfp.write(json.dumps(data, sort_keys = True, indent = 4))
 
+# This function will gather maximun of 1k all time submissions and comments of a redditor on any subreddit
 def redditorPost(userID, limitTo):
-  redditorPost = {}
-  redditorPost[userID] = []
+  redditor = {}
+  redditor[userID] = [{}]
+  redditor[userID][0]['Post'] = []
+  redditor[userID][0]['Comment'] = []
   try:
     submissions = reddit.redditor(userID).submissions.top(limit = limitTo)
     for submission in submissions:
-      redditorPost[userID].append(submission.selftext)
+      redditor[userID][0]['Post'].append(submission.selftext)
+    comments = reddit.redditor(userID).comments.top(limit = limitTo)
+    for comment in comments:
+      redditor[userID][0]['Comment'].append(comment.body)   
   except:
-    redditorPost[userID].append("404 Error")
+    redditor[userID][0]['Post'].append("Page Doesn't Exist Anymore")
+    redditor[userID][0]['Comment'].append("Page Doesn't Exist Anymore")
   finally:
-    writeData("{}.txt".format(userID),redditorPost)
+    writeData("{}.txt".format(userID),redditor)
 
+# limitToSubreddit will limit the number of submissions to go through in a subreddit
+# limitToRedditor will limit the number of submissions and comments to go through in a redditor
 def Subreddit(name, limitToSubreddit, limitToRedditor):
   subreddit = reddit.subreddit(name)
   submissions = subreddit.top(limit = limitToSubreddit)
   redditors = {}
   redditors['redditors'] = []
- 
   for submission in submissions:
     submission.comments.replace_more(limit= limitToSubreddit)
     redditors['redditors'].append(str(submission.author))
     for comment in submission.comments.list():
       redditors['redditors'].append(str(comment.author))
-  # get rid of repeated redditor
-  redditors['redditors'] = list(set(redditors['redditors']))
-   
+  # set will get rid of repeated redditors 
+  redditors['redditors'] = list(set(redditors['redditors'])) 
   for user in redditors['redditors']:
-    print("Submissions of : " + user)
     redditorPost(user, limitToRedditor)
   
 if __name__ == '__main__':
-  Subreddit('opiates', None, None)
+  Subreddit('Subreddit_Name', None, None)
